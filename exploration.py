7608d033fb4227144d5f3e836bcf9336504df3f3
Valentin Stamate <stamatevalentin125@gmail.com>
2021-10-10 01:31:28 +0300

    menu

---
+++ exploration.py
@@ -0,0 +1,53 @@
+from state import State
+
+
+class Exploration:
+
+    @staticmethod
+    def BackTracking(state):
+        return None
+
+    @staticmethod
+    def BFS(state):
+        return None
+
+    @staticmethod
+    def DFS(state):
+        return None
+
+    @staticmethod
+    def HillClimbing(state):
+        return None
+
+    @staticmethod
+    def AStar(state):
+        return None
+
+    @staticmethod
+    def start():
+        print("""
+        Backtracking = 1
+        BFS          = 2
+        DFS          = 3
+        HillClimbing = 4
+        AStar        = 5
+        """)
+
+        choice = int(input("Enter your choise: "))
+        couples = int(input("The number of couples: "))
+
+        state = State(couples)
+        if choice == 1:
+            Exploration.BackTracking(state)
+
+        if choice == 2:
+            Exploration.BFS(state)
+
+        if choice == 3:
+            Exploration.DFS(state)
+
+        if choice == 4:
+            Exploration.HillClimbing(state)
+
+        if choice == 5:
+            Exploration.AStar(state)
