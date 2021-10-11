8ec02d3978efcc808e3052b6a5e366c0ee23c3af
Laurentiu Mihai <mihai.laurentiu24@yahoo.com>
2021-10-11 18:23:41 +0300

    Update BacktrakingSolution.py

--- BacktrakingSolution.py
+++ BacktrakingSolution.py
@@ -1,59 +1,73 @@
 import copy
+import sys
+import time
+
 
 from state import State
+import resource, sys
+
+resource.setrlimit(resource.RLIMIT_STACK, (2 ** 29, -1))
+sys.setrecursionlimit(10 ** 6)
+
+
+def sol_print(solution):
+    for x in solution:
+        print(x)
+
+
+def print_river(states):
+    for state in states:
+        for x in range(len(state)):
+            if state[x] == 0:
+                print("* | |  ")
+            else:
+                print("  | | *")
+        time.sleep(1)
+        import os
+        os.system('cls')
 
 
 class BKTSolution:
     def __init__(self, state):
         self.state = state
+        self.best_solution = []
         self.solution = []
-        self.BKT(self.state, None, self.state.boat)
+        self.previous_states = [[0, 0, 0, 0, 0, 0]]
+        self.best_len = 100
+        self.BKT(self.state)
+        print_river(self.best_solution)
 
-    def BKT(self, state, previous_state, boat_side):
-        if state.is_solution():
-            print(self.solution)
+    def BKT(self, current_state):
+        if current_state.is_solution():
+            if len(self.solution) < self.best_len:
+                self.best_len = len(self.solution)
+                self.best_solution = copy.deepcopy(self.solution)
         else:
-            for p1 in range(len(state.positions)):
-                for p2 in range(len(state.positions)):
-                    for t1 in range(2):
-                        for t2 in range(2):
-                            if p1 != p2 or (p1 == p2 and t1 != t2):
-                                status = state.valid_transition(p1, t1)
-                                previous_state = copy.deepcopy(state.positions)
-                                if status is not None and status != previous_state:
-                                    state = status
-                                    print(state.positions)
-                                    self.solution.append(
-                                        {"side-from": boat_side, "index1": p1, "type1": t1})
-                                    self.state.boat = 1 - self.state.boat
-                                    self.BKT(state, previous_state, boat_side)
-                                    # self.solution.remove(
-                                    #     {"side-from": boat_side, "index1": p1, "type1": t1})
-                                status = state.valid_transition(p2, t2)
-                                if status is not None and status != previous_state:
-                                    state = status
-                                    print(state.positions)
-                                    self.solution.append(
-                                        {"side-from": boat_side, "index1": p2, "type1": t2})
-                                    self.state.boat = 1 - self.state.boat
-                                    self.BKT(state, previous_state, boat_side)
-                                    # self.solution.remove(
-                                    #     {"side-from": boat_side, "index1": p2, "type1": t2})
-                                status = state.valid_transition(p1, t1, p2, t2)
-                                if status is not None and status != previous_state:
-                                    state = status
-                                    print(state.positions)
-                                    self.solution.append(
-                                        {"side-from": boat_side, "index1": p1, "type1": t1,
-                                         "index2": p2, "type2": t2})
-                                    self.state.boat = 1 - self.state.boat
-                                    self.BKT(state, previous_state, boat_side)
-                                    # self.solution.remove(
-                                    #     {"side-from": boat_side, "index1": p1, "type1": t1,
-                                    #      "index2": p2, "type2": t2})
-
-    def print_transition(self, boat_side, p1, t1, p2, t2):
-        print()
+            for p1 in range(self.state.length):
+                for p2 in range(p1 + 1, self.state.length):
+                    self.previous_states.append(current_state.positions)
+                    new_state = State.valid_transition(current_state, p1, p2)
+                    if new_state is not None:
+                        if new_state.positions not in self.previous_states:
+                            self.solution.append(current_state.positions)
+                            self.BKT(new_state)
+                            self.solution.pop()
+                            self.previous_states.pop()
+                    new_state = State.valid_transition(current_state, p2)
+                    if new_state is not None:
+                        if new_state.positions not in self.previous_states:
+                            self.solution.append(current_state.positions)
+                            self.BKT(new_state)
+                            self.solution.pop()
+                            self.previous_states.pop()
+                    new_state = State.valid_transition(current_state, p1)
+                    if new_state is not None:
+                        if new_state.positions not in self.previous_states:
+                            self.solution.append(current_state.positions)
+                            self.BKT(new_state)
+                            self.solution.pop()
+                            self.previous_states.pop()
+                    self.previous_states.pop()
 
 
 if __name__ == "__main__":
