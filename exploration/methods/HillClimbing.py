ed00417d4dac2bb52aefd2626e31aa1f1114800b
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2021-10-18 10:30:53 +0300

    fixed hillclimbing to find solution

--- exploration/methods/HillClimbing.py
+++ exploration/methods/HillClimbing.py
@@ -11,20 +11,22 @@ def hill_climbing(state):
 
     ok = True
     while not state.is_solution() and ok:
-        ok = False
-        print(state.positions)
-        visited.add(state.as_key())
         max_f = 0
+        ok = False
+        visited.add(state.as_key())
         neighbours = State.neighbours(state)
         for neighbour in neighbours:
             key = neighbour.as_key()
-            if key not in visited and f(neighbour) > max_f:
+            if key not in visited and f(neighbour) + (1 - neighbour.boat) > max_f:
                 max_f = f(neighbour)
-                parents[key] = state
-                state = neighbour
+                new_key = key
+                new_neighbour = neighbour
                 ok = True
+        parents[new_key] = state
+        state = new_neighbour
         if state.is_solution():
             return state, parents
+
     print("Complete solution not found")
     print("Best solution found is:")
     return state, parents
