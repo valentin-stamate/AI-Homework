e250900e6f96a729f2334b92346d27ed7703533f
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2021-10-19 22:53:11 +0300

    Update HillClimbing.py

--- exploration/methods/HillClimbing.py
+++ exploration/methods/HillClimbing.py
@@ -2,7 +2,7 @@ from state import State
 
 
 def f(state):
-    return len([x for x in state.positions if x == 1])
+    return len([x for x in state.positions if x == 1]) - state.boat
 
 
 def hill_climbing(state):
@@ -10,18 +10,21 @@ def hill_climbing(state):
     parents = dict()
 
     ok = True
+    max_f = 0
     while not state.is_solution() and ok:
-        max_f = 0
         ok = False
+        print(state.positions)
         visited.add(state.as_key())
         neighbours = State.neighbours(state)
         for neighbour in neighbours:
             key = neighbour.as_key()
-            if key not in visited and f(neighbour) + (1 - neighbour.boat) > max_f:
+            if key not in visited and f(neighbour) >= max_f:
                 max_f = f(neighbour)
                 new_key = key
                 new_neighbour = neighbour
                 ok = True
+        if not ok:
+            break
         parents[new_key] = state
         state = new_neighbour
         if state.is_solution():
