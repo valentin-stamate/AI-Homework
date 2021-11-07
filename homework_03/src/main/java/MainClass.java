f2b6af4d164ef2379ec17ae6b6d3dba65e681637
Valentin Stamate <stamatevalentin125@gmail.com>
2021-11-07 22:28:34 +0200

    probably the final solution of minimax

--- homework_03/src/main/java/MainClass.java
+++ homework_03/src/main/java/MainClass.java
@@ -1,6 +1,10 @@
+import java.util.List;
+
 public class MainClass {
     public static void main(String[] args) {
-        MasterMind masterMind = new MasterMind(6, 10, 4);
-        masterMind.startGame();
+        MasterMind masterMind = new MasterMind(4, 10, 4);
+//        masterMind.startGame();
+        masterMind.startMinimax();
     }
+
 }
--- homework_03/src/main/java/MasterMind.java
+++ homework_03/src/main/java/MasterMind.java
@@ -77,7 +77,7 @@ public class MasterMind {
         return true;
     }
 
-    public List<String> choosePieces() {
+    public List<String> randomSequence() {
         Random random = new Random();
         List<String> sequence = new ArrayList<>();
 
@@ -99,6 +99,57 @@ public class MasterMind {
 
         subtractSequence(sequence);
         return sequence;
+    }
+
+    public boolean sequenceIsValid(List<String> sequence) {
+        for (String color : sequence) {
+            if (availablePieces[colorsMap.get(color)] == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public List<List<String>> generateMoves() {
+        List<List<String>> moves = new ArrayList<>();
+        String[] buffer = new String[sequence.size()];
+
+        allMoves(0, buffer, moves);
+
+        return moves;
+    }
+
+    public void allMoves(int position, String[] move, List<List<String>> list) {
+        int n = sequence.size();
+
+        if (position == n) {
+            if (sequenceIsValid(asList(move))) {
+                list.add(asList(move));
+            }
+            return;
+        }
+
+        for (String c : colors) {
+            if (!sequenceContainsColor(move, position, c)) {
+                move[position] = c;
+                allMoves(position + 1, move, list);
+            }
+        }
+    }
+
+    private List<String> asList(String[] list) {
+        return new ArrayList<>(Arrays.asList(list));
+    }
+
+    private boolean sequenceContainsColor(String[] list, int len, String color) {
+        for (int i = 0; i < len; i++) {
+            if (list[i].equals(color)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public int compareSequence(List<String> playerSequence) {
@@ -246,9 +297,138 @@ public class MasterMind {
         }
     }
 
+    public void startMinimax() {
+        List<String> currentMove = randomSequence();
+        List<String> bestMove = new ArrayList<>();
+
+        List<List<String>> previousMoves = new ArrayList<>();
+        previousMoves.add(new ArrayList<>(currentMove));
+
+        boolean solutionFound = false;
+
+        for (int i = 2 * n; i >= 1; i--) {
+            minimax(1, currentMove, previousMoves, true, Integer.MIN_VALUE, Integer.MAX_VALUE, bestMove);
+
+            currentMove = bestMove;
+            previousMoves.add(new ArrayList<>(currentMove));
+
+            if (isSolution(currentMove)) {
+                solutionFound = true;
+                break;
+            }
+        }
+
+        if (solutionFound) {
+            System.out.println("Solution found");
+        }
+
+        System.out.println(sequence);
+        System.out.println();
+
+        for (var list : previousMoves) {
+            System.out.println(list);
+        }
+
+    }
+
+    public int minimax(int depth, List<String> sequence, List<List<String>> previousMoves, boolean maximizing, int alpha, int beta, List<String> bestMove) {
+        if (depth == 0) {
+            return depth + compareSequence(sequence);
+        }
+
+        if (compareSequence(sequence) == sequence.size()) {
+            return depth + sequence.size();
+        }
+
+        List<List<String>> moves = generateMoves();
+
+        if (maximizing) {
+            int max = Integer.MIN_VALUE;
+
+            for (var move : moves) {
+                boolean found = false;
+
+                for (List<String> list : previousMoves) {
+                    if (equalSequences(move, list)) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found) {
+                    continue;
+                }
+
+                subtractSequence(move);
+                /* maximizing should be false, but we have only one player */
+                int score = minimax(depth - 1, move, previousMoves, true, alpha, beta, bestMove);
+                undoSequence(move);
+
+                if (max < score) {
+                    max = score;
+
+                    bestMove.clear();
+                    bestMove.addAll(move);
+                }
+
+                alpha = Math.max(alpha, score);
+
+                if (beta <= alpha) {
+                    break;
+                }
+
+            }
+
+            return max;
+        }
+
+        System.out.println("Something went wrong");
+        /* minimizing */
+        int min = Integer.MAX_VALUE;
+
+        for (var move : moves) {
+
+            subtractSequence(move);
+            int score = minimax(depth - 1, move, previousMoves, true, alpha, beta, bestMove);
+            undoSequence(move);
+
+            if (min > score) {
+                min = score;
+
+                bestMove.clear();
+                bestMove.addAll(move);
+            }
+
+            beta = Math.min(beta, score);
+
+            if (beta <= alpha) {
+                break;
+            }
+        }
+
+        return min;
+
+    }
+
+    private boolean equalSequences(List<String> a, List<String> b) {
+        for (int i = 0; i < a.size(); i++) {
+            if (!a.get(i).equals(b.get(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     private void subtractSequence(List<String> colors) {
         for (String color : colors) {
             availablePieces[colorsMap.get(color)]--;
+        }
+    }
+
+    private void undoSequence(List<String> colors) {
+        for (String color : colors) {
+            availablePieces[colorsMap.get(color)]++;
         }
     }
 
