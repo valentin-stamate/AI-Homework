5ba83bc74fca6280575b0b9fc79eed0046ffc65f
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2021-12-05 12:21:12 +0200

    imported nltk

--- homework_05/main.py
+++ homework_05/main.py
@@ -1,17 +1,29 @@
 import lightrdf as lightrdf
+import nltk
 
 
 def get_all_super_topic_of(keyword):
     # parser = lightrdf.Parser()
-    doc = lightrdf.RDFDocument('ontology.owl')
+    keyword = input("Insert a word:")
+    doc = lightrdf.RDFDocument('Resources/ontology.owl')
     for triple in doc.search_triples(None, None, None):
         if triple[2].endswith(keyword) and 'superTopicOf' in triple[1]:
             print(triple)
 
 
+def extract_sentences():
+    # f = open("Resources/text.txt", 'r')
+    # data = f.readlines()
+    data = "I bought milk yesterday"
+    tokens = nltk.word_tokenize(data)
+    tagged_tokens = nltk.pos_tag(tokens)
+    print(tagged_tokens)
+
+
 def main():
-    keyword = input("Insert a word:")
-    get_all_super_topic_of(keyword)
+    # keyword = input("Insert a word:")
+    # get_all_super_topic_of(keyword)
+    extract_sentences()
 
 
 if __name__ == '__main__':
